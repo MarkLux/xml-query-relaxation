92e0743d614ebd4b029abbedd0515f6a751b8718
远岩 <yuanyan.lmh@alibaba-inc.com>
2018-11-19 10:15:27 +0800

    save

--- test/seed.py
+++ test/seed.py
@@ -16,13 +16,6 @@ class AttrMock(object):
         self.rang = rang
 
 general_attr_def = {
-    'type': AttrMock('type', 'c', [
-        'rain',
-        'sunny',
-        'cloudy',
-        'snowy',
-        'windy'
-    ]),
     'temperature': AttrMock('temperature', 'n', [10.0, 20.0]),
     'humidity': AttrMock('humidity', 'n', [0.4, 0.7]),
     'windForce': AttrMock('windForce', 'n', [3.0, 6.0]),
@@ -63,10 +56,67 @@ position_data_mock = {
     'The Temple of Heaven': {
         'district': 'DongCheng',
         'location': (116.417246,39.888243)
+    },
+    'Temple of Confucius' : {
+        'district': 'DongCheng',
+        'location': (39.9461390230,116.4150411626)
+    },
+    'The Imperial College': {
+        'district': 'DongCheng',
+        'location': (39.9468822007,116.4132395326)
+    },
+    'Ming Cheng Wall Ruins Park ': {
+        'district': 'DongCheng',
+        'location': (39.9010525899,116.4280396238)
+    },
+    'Tian An Men': {
+        'district': 'XiCheng',
+        'location': (39.9088596409,116.3975157338)
+    },
+    'Altar of the Moon Park': {
+        'district': 'XiCheng',
+        'location': (39.9166844333,116.3524948131)
+    },
+    'Bei Hai Park': {
+        'district': 'XiCheng',
+        'location': (39.9275995766,116.3887407049)
+    },
+    'Shi Sha Lake': {
+        'district': 'XiCheng',
+        'location': (39.9405859540,116.3884021293)
+    },
+    'Jing Shan Park': {
+        'district': 'XiCheng',
+        'location': (22.2636597551,113.5793713618)
+    },
+    'South Luogu Lane': {
+        'district': 'ChaoYang',
+        'location': (22.2636597551,113.5793713618)
+    },
+    'Aquaria of Beijing': {
+        'district': 'ShunYi',
+        'location': (22.2636597551,113.5793713618)
+    },
+    'Xiang Shan Park': {
+        'district': 'HaiDian',
+        'location': (22.2636597551,113.5793713618)
+    },
+    'Geological Museum Of China': {
+        'district': 'HaiDian',
+        'location': (39.9915586721,116.3452162875)
     }
 }
 
-def generate_node(doc_root, id, postion='', date='', district='', location=None, time=None):
+district_mock = {
+    'YanQing': ['sunny', 'windy'],
+    'HaiDian': ['sunny', 'windy'],
+    'DongCheng': ['rain', 'cloudy', 'windy'],
+    'ShunYi': ['windy', 'snowy'],
+    'XiCheng': ['sunny', 'cloudy'],
+    'ChaoYang': ['windy', 'cloudy']
+}
+
+def generate_node(doc_root, id, postion='', date='', district='', w='', location=None, time=None):
     random_seed = random.random()
     row_ele = ET.SubElement(doc_root, 'wheather', {'id': str(id), 'prob': str(random_seed)})
     # 创建general节点
@@ -77,6 +127,8 @@ def generate_node(doc_root, id, postion='', date='', district='', location=None,
     ds_e.text = district
     d_e = ET.SubElement(general_ele, 'date')
     d_e.text = date
+    ty_e = ET.SubElement(general_ele, 'type')
+    ty_e.text = w 
     # 创建时空节点
     sp_ele = ET.SubElement(row_ele, 'spatioTemporal')
     l_e = ET.SubElement(sp_ele, 'space', {'x': str(location[0]), 'y': str(location[1])})
@@ -105,13 +157,26 @@ if __name__ == "__main__":
         'The Summer Palace',
         'Lama Temple',
         'Garden of Gardens',
-        'The Temple of Heaven'
+        'The Temple of Heaven',
+        'Temple of Confucius',
+        'The Imperial College',
+        'Ming Cheng Wall Ruins Park ',
+        'Tian An Men',
+        'Altar of the Moon Park',
+        'Bei Hai Park',
+        'Shi Sha Lake',
+        'Jing Shan Park',
+        'South Luogu Lane',
+        'Aquaria of Beijing',
+        'Xiang Shan Park',
+        'Geological Museum Of China'
     ]
     date = [
         '20181116',
         '20181117',
         '20181118',
-        '20181119'
+        '20181119',
+        '20181120',
     ]
     time = [
         (0, 2),
@@ -134,8 +199,11 @@ if __name__ == "__main__":
             # 枚举时间
             for t in time:
                 i += 1
+                district = position_data_mock.get(p).get('district')
                 generate_node(doc_root, i, postion=p, date=d, time=t, 
-                district=position_data_mock.get(p).get('district'), location=position_data_mock.get(p).get('location'))
+                district=district, 
+                w=random.choice(district_mock.get(district)),
+                location=position_data_mock.get(p).get('location'))
 
     output_xml(doc_root, 'result.xml')
     
